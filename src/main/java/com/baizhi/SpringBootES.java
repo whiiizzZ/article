3b2d2d5715b5b4962a86bff8755a25ef0fcb3699
wh <1242271439@qq.com>
2019-12-01 16:07:15 +0800

    add sout

--- src/main/java/com/baizhi/SpringBootES.java
+++ src/main/java/com/baizhi/SpringBootES.java
@@ -9,5 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootES {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootES.class, args);
+        System.out.println();
     }
 }
